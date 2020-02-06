537bdca7a936bb1e468d1635bcedb4f3c75a1fb3
Gannon M Carroll <gmc384@humboldt.edu>
2020-02-06 09:10:56 -0800

    All page working with splitext() to return just the root of the file so the page redirects work

--- app/routes.py
+++ app/routes.py
@@ -20,7 +20,8 @@ def all():
     #TODO: figure out how to find all files 
     #in the app
     view_data = {}
-    view_data['pages']= listdir("C:\\Users\\Gannon\\Desktop\\bin\\flask-blog\\app\\templates")
+    view_data['pages']= listdir("app\\templates")
+    allsplit(view_data)
     return render_template("all.html", data = view_data)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -29,6 +30,15 @@ def login():
         #TODO: process request.values as necessary
         session['user_name'] = request.values['user_name']
     return ""
+
+def allsplit(view_data):
+    i = 0
+    while i < len(view_data['pages']):
+        temp = {}
+        temp = splitext(view_data['pages'][i])
+        view_data['pages'][i] = temp[0]
+        i+=1
+    return view_data
 
 @app.route("/favicon.ico")
 def favicon():
